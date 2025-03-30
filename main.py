3d87a9072f631c798d2e20cc1289c326f30ac242
wenjli <liwenjin749@gmail.com>
2025-03-30 21:38:51 +0800

    add run examples

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler("daily_news_process.log"),
+        logging.FileHandler("daily_news_process.log", encoding='utf-8'),
         logging.StreamHandler()
     ]
 )
